1d919e2e5b101b31d6a23550e49e1ca0555ed467
sata <lesmanasata@gmail.com>
2021-04-17 12:39:36 +0700

    make it better

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'https://app-store-ci.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
