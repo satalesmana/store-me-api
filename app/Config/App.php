2396480f017727c63b9fea45352aae31e6650f6d
sata <lesmanasata@gmail.com>
2021-04-17 16:59:07 +0700

    env config

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://app-store-ci.herokuapp.com/';
+	public $baseURL = '';
 
 	/**
 	 * --------------------------------------------------------------------------
