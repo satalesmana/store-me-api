88e9f4c1e670c9be45ac15ba13d0573454b251a8
Rifkah Azzahra Sukma <82221162+sukmaarifkah@users.noreply.github.com>
2021-06-03 15:05:03 +0700

    App: Initial profile page

--- app/Views/app/profile.php
+++ app/Views/app/profile.php
@@ -1,4 +1,50 @@
 <!-- Profile (Sukma) -->
-<div class="container-fluid p-0 m-0 page" id="profile-page">
+<div class="container-md text-center" id="profile-page">
+	<br/><br/>
+	<img src="https://www.maxpixel.net/static/photo/640/Girl-Woman-Young-Avatar-Muslim-Female-Hijab-1606914.png" width="100">
+	<h5 class="text-center">Rifkah Azzahra Sukma</h5>
+	<p class="lead">@rifkahazzahra9</p>	
 </div>
+
+<div class="container-md" id="profile-page">
+<div class="accordion shadow-sm rounded" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Akun Saya
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <a class="dropdown-item" href="#">Profil</a>
+        <a class="dropdown-item" href="#">Alamat</a>
+        <a class="dropdown-item" href="#">Ubah Password</a>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Pesanan Saya
+      </button>
+    </h2>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Notifikasi
+      </button>
+    </h2>
+  </div>
+ <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        Voucher
+      </button>
+    </h2>
+  </div>
+</div>
+</div>
+
+
 <!-- End profile -->
